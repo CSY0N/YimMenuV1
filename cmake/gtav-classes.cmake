3e474222376f73d0cbaab2997a21f72712e81ed1
CSY0N <134310111+CSY0N@users.noreply.github.com>
2025-06-21 16:12:47 +0200

    Update gtav-classes.cmake
    GIT_TAG will be Updated later

--- cmake/gtav-classes.cmake
+++ cmake/gtav-classes.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(
     gtav_classes
-    GIT_REPOSITORY https://github.com/Mr-X-GTA/GTAV-Classes-1.git
+    GIT_REPOSITORY https://github.com/CSY0N/GTAV-Classes.git
     GIT_TAG        03bc35cc7e61eb74919ad51c217f6b4bf5502088
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
